d1cd1cd017d4616560a5e6164c7c8acb4d8165b1
jedrazb <jedrazb@gmail.com>
2018-10-17 17:35:00 +0100

    Setting CI for Java project

---
+++ backend/src/test/java/eu/mhutti1/healthchain/MainTest/MainTest.java
@@ -0,0 +1,16 @@
+package eu.mhutti1.healthchain.MainTest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by jedraz on 17/10/2018.
+ */
+public class MainTest {
+
+  @Test
+  public void testTest() {
+    Assert.assertTrue(1 == 1);
+  }
+
+}
