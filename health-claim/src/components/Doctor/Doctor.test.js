03bec062169fb268a98b4f9e781b674f38536689
Boris Barath <boris.barath16@imperial.ac.uk>
2018-10-18 11:14:19 +0100

    Added basic tests

---
+++ health-claim/src/components/Doctor/Doctor.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import Doctor from "./Doctor";
+
+describe("Doctor", () => {
+  it("should render correctly with mocked records", () => {
+    const tree = renderer.create(<Doctor />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
