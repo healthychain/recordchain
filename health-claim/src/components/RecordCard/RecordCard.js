43d9c05a9f1c496b17eea5c3d8988514e01de955
Boris Barath <boris.barath16@imperial.ac.uk>
2018-10-18 12:36:42 +0100

    Further refactor

--- health-claim/src/components/RecordCard/RecordCard.js
+++ health-claim/src/components/RecordCard/RecordCard.js
@@ -2,19 +2,17 @@ import React, { Component } from "react";
 import "./RecordCard.css";
 import { Card, CardText, CardBody, CardTitle, Button } from "reactstrap";
 
-class RecordCard extends Component {
-  render() {
-    const { record, onClick } = this.props;
-    return (
-      <Card className="card" onClick={onClick}>
-        <CardBody>
-          <CardTitle>{record.title}</CardTitle>
-          <CardText>{record.description}</CardText>
-          <Button color="link">Open the record</Button>
-        </CardBody>
-      </Card>
-    );
-  }
-}
+const RecordCard = props => {
+  const { record, onClick } = props;
+  return (
+    <Card className="card" onClick={onClick}>
+      <CardBody>
+        <CardTitle>{record.title}</CardTitle>
+        <CardText>{record.description}</CardText>
+        <Button color="link">Open the record</Button>
+      </CardBody>
+    </Card>
+  );
+};
 
 export default RecordCard;
