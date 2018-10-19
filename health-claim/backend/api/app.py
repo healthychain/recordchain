01ad53819880d387514177341b35401e8bb27a70
Matej Novak <matej@dyn3173-171.wlan.ic.ac.uk>
2018-10-19 14:01:10 +0100

    Cors enabled

--- health-claim/backend/api/app.py
+++ health-claim/backend/api/app.py
@@ -1,7 +1,13 @@
 #!flask/bin/python
 from flask import Flask, jsonify, make_response, abort, request
+from flask_cors import CORS, cross_origin
+
 
 app = Flask(__name__)
+
+app.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(app)
+
 
 claims = [
     {
@@ -66,10 +72,12 @@ def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 @app.route('/api/claims', methods=['GET'])
+@cross_origin()
 def get_claims():
     return jsonify({'claims': claims})
 
 @app.route('/api/claims/<int:claim_id>', methods=['GET'])
+@cross_origin()
 def get_claim(claim_id):
     claim = [claim for claim in claims if claim['id'] == claim_id]
     if len(claim) == 0:
@@ -81,6 +89,7 @@ def get_claim(claim_id):
 
 
 @app.route('/api/patients-claims/<int:patient_id>', methods=['GET'])
+@cross_origin()
 def get_patients_claim(patient_id):
     claim = [claim for claim in claims if claim['patient'] == patient_id]
     if len(claim) == 0:
@@ -93,6 +102,7 @@ def get_patients_claim(patient_id):
 
 
 @app.route('/api/claims', methods=['POST'])
+@cross_origin()
 def create_claim():
     if not request.json or not 'title' in request.json:
         abort(400)
@@ -107,6 +117,7 @@ def create_claim():
 
 
 @app.route('/api/<int:claim_id>', methods=['PUT'])
+@cross_origin()
 def update_claim(claim_id):
     claim = [claim for claim in claims if claim['id'] == claim_id]
     if len(claim) == 0:
@@ -122,6 +133,7 @@ def update_claim(claim_id):
     return jsonify({'claim': claim[0]})
 
 @app.route('/api/claim/<int:claim_id>', methods=['DELETE'])
+@cross_origin()
 def delete_claim(claim_id):
     claim = [claim for claim in claims if claim['id'] == claim_id]
     if len(claim) == 0:
