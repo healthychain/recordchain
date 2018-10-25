cd797e1bc35cd0dcc53fbfc33af592a6be98493f
Matej Novak <matej@dyn3163-234.wlan.ic.ac.uk>
2018-10-25 14:12:45 +0100

    Initial commit to branch

--- backend/src/main/java/eu/mhutti1/healthchain/Main.java
+++ backend/src/main/java/eu/mhutti1/healthchain/Main.java
@@ -21,6 +21,24 @@ import org.hyperledger.indy.sdk.wallet.Wallet;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import org.hyperledger.indy.sdk.did.Did;
+import org.hyperledger.indy.sdk.did.DidJSONParameters;
+import org.hyperledger.indy.sdk.did.DidResults;
+import org.hyperledger.indy.sdk.pool.Pool;
+import org.hyperledger.indy.sdk.wallet.Wallet;
+import org.json.JSONObject;
+//import utils.PoolUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//import static org.hyperledger.indy.sdk.anoncreds.Anoncreds.issuerCreateAndStoreClaimDef;
+import static org.hyperledger.indy.sdk.ledger.Ledger.*;
+//import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertNotNull;
+
+
+
 public class Main {
 
 
@@ -28,24 +46,103 @@ public class Main {
 
     Pool pool = getPool();
     Wallet wallet = getWallet();
+//
+//    //this is hardcoded steward from genesis transaction file
+//    String did_json = "{\"seed\": \"" + STEWARD_SEED + "\"}";
+//    DidResults.CreateAndStoreMyDidResult stewardResult = Did.createAndStoreMyDid(wallet, did_json).get();
+//
+//    Steward steward = new Steward(wallet, stewardResult.getDid(), stewardResult.getVerkey());
+    //CALL ALL OF THESE ABOVE EVERY TIME
 
-    //this is hardcoded steward from genesis transaction file
+//    TrustAnchor doctor = new TrustAnchor(pool, steward);
+//
+//    IdentityOwner client = new IdentityOwner(pool, doctor);
+//
+//    String getNymRequest = buildGetNymRequest(client.getDid(), doctor.getDid()).get();
+//    String getNymResponse = submitRequest(pool, getNymRequest).get();
+//
+//    String responseData = new JSONObject(getNymResponse).getJSONObject("result").getString("data");
+//    String trustAnchorVerkeyFromLedger = new JSONObject(responseData).getString("verkey");
+//
+//    System.out.println("Written by Steward: " + doctor.getVerKey());
+//    System.out.println("Queried from Ledger: " + trustAnchorVerkeyFromLedger);
+
+
+    //ROTATE KEY CODE
+    //------------------------------------------------------------------------
+    //------------------------------------------------------------------------
+
+//    String walletName = "myWallet";
+//    String poolName = "pool";
+//    String stewardSeed = "000000000000000000000000Steward1";
+//    String seed_trustanchor = "TestTrustAnchor00000000000000000";
+//    String poolConfig = "{\"genesis_txn\": \"/home/vagrant/code/evernym/indy-sdk/cli/docker_pool_transactions_genesis\"}";
+
+
+    // Step 2 code goes here.
+
+    // Tell SDK which pool you are going to use. You should have already started
+    // this pool using docker compose or similar. Here, we are dumping the config
+    // just for demonstration purposes.
+    System.out.println("\n1. Creating a new local pool ledger configuration that can be used later to connect pool nodes.\n");
+    //Pool.createPoolLedgerConfig(poolName, poolConfig).get();
+
+    System.out.println("\n2. Open pool ledger and get the pool handle from libindy.\n");
+    //Pool pool = Pool.openPoolLedger(poolName, "{}").get();
+
+    System.out.println("\n3. Creates a new secure wallet\n");
+    //Wallet.createWallet(WALLET_CONFIG, WALLET_CREDS).get();
+
+    System.out.println("\n4. Open wallet and get the wallet handle from libindy\n");
+    Wallet walletHandle = wallet;
+
+    // First, put a steward DID and its keypair in the wallet. This doesn't write anything to the ledger,
+    // but it gives us a key that we can use to sign a ledger transaction that we're going to submit later.
+    System.out.println("\n5. Generating and storing steward DID and Verkey\n");
+
+    // The DID and public verkey for this steward key are already in the ledger; they were part of the genesis
+    // transactions we told the SDK to start with in the previous step. But we have to also put the DID, verkey,
+    // and private signing key into our wallet, so we can use the signing key to submit an acceptably signed
+    // transaction to the ledger, creating our *next* DID (which is truly new). This is why we use a hard-coded seed
+    // when creating this DID--it guarantees that the same DID and key material are created that the genesis txns
+    // expect.
     String did_json = "{\"seed\": \"" + STEWARD_SEED + "\"}";
-    DidResults.CreateAndStoreMyDidResult stewardResult = Did.createAndStoreMyDid(wallet, did_json).get();
+    DidResults.CreateAndStoreMyDidResult stewardResult = Did.createAndStoreMyDid(walletHandle, did_json).get();
+    String defaultStewardDid = stewardResult.getDid();
+    System.out.println("Steward did: " + defaultStewardDid);
 
-    Steward steward = new Steward(wallet, stewardResult.getDid(), stewardResult.getVerkey());
-    TrustAnchor doctor = new TrustAnchor(pool, steward);
+    // Now, create a new DID and verkey for a trust anchor, and store it in our wallet as well. Don't use a seed;
+    // this DID and its keyas are secure and random. Again, we're not writing to the ledger yet.
+    System.out.println("\n6. Generating and storing Trust Anchor DID and Verkey\n");
+    DidResults.CreateAndStoreMyDidResult trustAnchorResult = Did.createAndStoreMyDid(walletHandle, "{}").get();
+    String trustAnchorDID = trustAnchorResult.getDid();
+    String trustAnchorVerkey = trustAnchorResult.getVerkey();
+    System.out.println("Trust anchor DID: " + trustAnchorDID);
+    System.out.println("Trust anchor Verkey: " + trustAnchorVerkey);
 
-    IdentityOwner client = new IdentityOwner(pool, doctor);
+    // Here, we are building the transaction payload that we'll send to write the Trust Anchor identity to the ledger.
+    // We submit this transaction under the authority of the steward DID that the ledger already recognizes.
+    // This call will look up the private key of the steward DID in our wallet, and use it to sign the transaction.
+    System.out.println("\n7. Build NYM request to add Trust Anchor to the ledger\n");
+    String nymRequest = buildNymRequest(defaultStewardDid, trustAnchorDID, trustAnchorVerkey, null, "TRUST_ANCHOR").get();
+    System.out.println("NYM request JSON:\n" + nymRequest);
 
-    String getNymRequest = buildGetNymRequest(client.getDid(), doctor.getDid()).get();
-    String getNymResponse = submitRequest(pool, getNymRequest).get();
+    // Now that we have the transaction ready, send it. The building and the sending are separate steps because some
+    // clients may want to prepare transactions in one piece of code (e.g., that has access to privileged backend systems),
+    // and communicate with the ledger in a different piece of code (e.g., that lives outside the safe internal
+    // network).
+    System.out.println("\n8. Sending NYM request to ledger\n");
+    String nymResponseJson = signAndSubmitRequest(pool, walletHandle, defaultStewardDid, nymRequest).get();
+    System.out.println("NYM transaction response:\n" + nymResponseJson);
 
-    String responseData = new JSONObject(getNymResponse).getJSONObject("result").getString("data");
-    String trustAnchorVerkeyFromLedger = new JSONObject(responseData).getString("verkey");
+    // At this point, we have successfully written a new identity to the ledger.
 
-    System.out.println("Written by Steward: " + doctor.getVerKey());
-    System.out.println("Queried from Ledger: " + trustAnchorVerkeyFromLedger);
+
+
+    // Step 3 code goes here.
+
+    //------------------------------------------------------------------------
+    //------------------------------------------------------------------------
 
   }
 
