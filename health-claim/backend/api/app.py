09882966cd4a92380ff181bd498def40f68f0844
Matej Novak <matej@dyn3163-234.wlan.ic.ac.uk>
2018-10-25 19:47:54 +0100

    login/logout of users functional

--- health-claim/backend/api/app.py
+++ health-claim/backend/api/app.py
@@ -1,12 +1,63 @@
 #!flask/bin/python
-from flask import Flask, jsonify, make_response, abort, request
+from flask import Flask, jsonify, make_response, abort, request, redirect
 from flask_cors import CORS, cross_origin
+from flask_login import LoginManager, login_required, login_user, UserMixin, logout_user
 
 
 app = Flask(__name__)
 
+# config
 app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['SECRET_KEY'] = 'yolo2018'
+
 cors = CORS(app)
+
+login_manager = LoginManager()
+
+
+class User(UserMixin):
+
+    def __init__(self, id):
+        self.id = id
+        self.name = "user" + str(id)
+
+    def __repr__(self):
+        return "%d/%s/%s" % (self.id, self.name)
+    
+users = [User(id) for id in range(1, 3)]
+
+
+doctors_credentials = [
+    {
+        'username': 'doctor1',
+        'password': 'psw1',
+        'id': 1
+    },
+    {
+        'username': 'doctor2',
+        'password': 'psw2',
+        'id': 2
+    }
+]
+
+patients_credentials = [
+    {
+        'username': 'patient1',
+        'password': 'psw1',
+        'id': 1
+    },
+    {
+        'username': 'patient2',
+        'password': 'psw2',
+        'id': 2
+    },
+    {
+        'username': 'patient3',
+        'password': 'psw3',
+        'id': 3
+    }
+]
+
 
 
 claims = [
@@ -66,6 +117,44 @@ claims = [
     }
 ]
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User(user_id)
+
+@app.route('/login_patient', methods=['POST'])
+def login_patient():
+    username = request.json['username']
+    password = request.json['password']
+    matching_patient = [patient for patient in patients_credentials if patient['username'] == username]
+    if len(matching_patient) > 0 and password == matching_patient[0]['password']:
+        user = User(username[-1])
+        print(username[-1])
+        login_user(user)
+        return make_response(jsonify({'result': 'success'}), 200)
+    else:
+        return abort(401)
+
+@app.route('/login_doctor', methods=['POST'])
+def login_doctor():
+    username = request.json['username']
+    password = request.json['password']
+    matching_doctor = [doctor for doctor in doctors_credentials if doctor['username'] == username]
+    if len(matching_doctor) > 0 and password == matching_doctor[0]['password']:
+        user = User(username[-1])
+        print(username[-1])        
+        login_user(user)
+        return make_response(jsonify({'result': 'success'}), 200)
+    else:
+        return abort(401)
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return make_response(jsonify({'result': 'success'}), 200)
+
+
+
 
 @app.errorhandler(404)
 def not_found(error):
@@ -76,6 +165,7 @@ def not_found(error):
 def get_claims():
     return jsonify({'claims': claims})
 
+
 @app.route('/api/claims/<int:claim_id>', methods=['GET'])
 @cross_origin()
 def get_claim(claim_id):
@@ -83,9 +173,6 @@ def get_claim(claim_id):
     if len(claim) == 0:
         abort(404)
     return jsonify({'claim': claim[0]})
-
-
-
 
 
 @app.route('/api/patients-claims/<int:patient_id>', methods=['GET'])
@@ -153,5 +240,7 @@ if __name__ == '__main__':
             port = int(server_name.rsplit(':', 1)[1])
         else:
             port = 5000
+
+    login_manager.init_app(app)
     app.run(host=host, port=port)
     #app.run(debug=True)
