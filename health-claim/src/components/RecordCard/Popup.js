e80cb3fa5c72e0cfd815f92ef0d6a469d083f02f
irinadanes <aid16@ic.ac.uk>
2018-11-07 22:21:52 +0000

    Creating table from json

--- health-claim/src/components/RecordCard/Popup.js
+++ health-claim/src/components/RecordCard/Popup.js
@@ -2,123 +2,77 @@ import React from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Table} from 'reactstrap';
 
 const record_data = [{
-    "name": "Irina Danes",
-    "age": "21",
-    "dob": "01/06/1997",
-    "address": "London",
-    "general": {
-        "heartbeat": "good",
-        "bloodpressure": "good",
-        "surgeries": "none"
-    },
-    "eyesight": "good",
-    "medications": "none",
-    "allergies": "none"
+    "Name": "Irina Danes",
+    "Age": "21",
+    "Date of Birth": "01/06/1997",
+    "Address": "London",
+    "Heartbeat": "good",
+    "Blood Pressure": "good",
+    "Surgeries": "none",
+    "Eyesight": "good",
+    "Medications": "none",
+    "Allergies": "none"
 }]
+
+var keys = Object.keys(record_data[0]);
+var values = Object.values(record_data[0]);
+
+var data = [];
+for (var i = 0; i < keys.length; i++) { 
+    var row = {"key": keys[i], "value": values[i]};
+    data.push(row);
+}
+
+var columns = [
+    { key: 'key', label: '' },    
+    { key: 'value', label: '' },
+];
 
 class ModalExample extends React.Component {
 
     constructor(props) {
-    super(props);
-    this.state = {
-      modal: false,
-      json: []
-    };
+        super(props);
+        this.state = {
+        modal: false,
+        json: []
+        };
+        this.toggle = this.toggle.bind(this);
+    }
 
-    this.toggle = this.toggle.bind(this);
-  }
-
-  toggle() {
-    this.setState({
-      modal: !this.state.modal
+    toggle() {
+        this.setState({
+        modal: !this.state.modal
     });
-  }
+    }
 
-  componentDidMount() {
-    this.setState((prevState) => {
-        return {
-            json: record_data
-        }
-    })
-}
-    
+    generateRows() {
+        return data.map(function(item) {
+            var cells = columns.map(function(colData) {
+                return <td> {item[colData.key]} </td>;
+            });
+        return <tr key={item.id}> {cells} </tr>;
+        });
+    }
 
-  render() {
-    return (
-      <div>
-        <Button color="link" onClick={this.toggle}>Open the record</Button>
-        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-          <ModalHeader toggle={this.toggle}>Health record 1</ModalHeader>
-          <ModalBody>
-
+    render() {
+        var rowComponents = this.generateRows();
+        return (
             <div>
+            <Button color="link" onClick={this.toggle}>Open the record</Button>
+            <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+            <ModalHeader toggle={this.toggle}><b>Health record 1</b></ModalHeader>
+            <ModalBody>
                 <Table hover>
-                    <tbody>
-                    {this.state.json.map((data, i) => {
-                        return (
-                            <div>
-                            <tr key={i}>
-                                <th scope="row">Name</th>
-                                <td>{data.name}</td>
-                                </tr>
-                                <tr>
-                                <th scope="row">Age</th>
-                                <td>{data.age}</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">Date of Birth</th>
-                                <td>{data.dob}</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">Address</th>
-                                <td>{data.address}</td>
-                            </tr>
-                            <tr>                            
-
-                                <th scope="row"><br />General</th>
-                            </tr>
-                            <tr>
-                                {/* <th scope="row"></th> */}
-                                <td>Heartbeat</td>
-                                <td>{data.general.heartbeat}</td>
-                            </tr>
-                            <tr>
-                                {/* <th scope="row"></th> */}
-                                <td>Blood Pressure</td>
-                                <td>{data.general.bloodpressure}</td>
-                            </tr>
-                            <tr>
-                                {/* <th scope="row"></th> */}
-                                <td>Surgeries</td>
-                                <td>{data.general.surgeries}</td>
-                            </tr>
-                            <tr>
-                                <td>Eyesight</td>
-                                <td>{data.eyesight}</td>
-                            </tr>
-                            <tr>
-                                <th scope="row"><br />Prescribed Medications</th>
-                                <td><br />{data.medications}</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">Allergies</th>
-                                <td>{data.allergies}</td>
-                            </tr>
-                            </div>
-                        )
-                    })}
-                </tbody>
-            </Table>
+                    <tbody> {rowComponents}</tbody> 
+                </Table>
+            </ModalBody>
+            <ModalFooter>
+                <Button color="primary" onClick={this.toggle}>Close</Button>
+            </ModalFooter>
+            </Modal>
             </div>
-
-          </ModalBody>
-          <ModalFooter>
-            <Button color="primary" onClick={this.toggle}>Close</Button>
-          </ModalFooter>
-        </Modal>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default ModalExample;
