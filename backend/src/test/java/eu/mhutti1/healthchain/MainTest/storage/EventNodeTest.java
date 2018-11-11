8116c6438d5a495c2a0bbab656eafc8d456b7044
jedrazb <jedrazb@gmail.com>
2018-11-11 17:58:10 +0000

    Tests of java agent added

---
+++ backend/src/test/java/eu/mhutti1/healthchain/MainTest/storage/EventNodeTest.java
@@ -0,0 +1,20 @@
+package eu.mhutti1.healthchain.MainTest.storage;
+
+import eu.mhutti1.healthchain.storage.EventNode;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import java.io.*;
+
+/**
+ * Created by jedraz on 11/11/2018.
+ */
+public class EventNodeTest {
+
+  @Test
+  public void canSerializeAndDeserializeItself() throws IOException {
+
+    // add some code to check if it works
+
+  }
+}
