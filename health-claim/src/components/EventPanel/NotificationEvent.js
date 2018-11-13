29f8f40fd5075f35d74408ee288edb0b274c856e
Boris Barath <boris.barath16@imperial.ac.uk>
2018-11-13 16:02:30 +0000

    notifications may require master secret

--- health-claim/src/components/EventPanel/NotificationEvent.js
+++ health-claim/src/components/EventPanel/NotificationEvent.js
@@ -2,20 +2,65 @@ import React, { Component } from "react";
 import "./EventPanel.scss";
 
 class NotificationEvent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { masterSecret: null };
+  }
+
   render() {
-    const { handle, acceptAction, events, token, id } = this.props;
+    const {
+      handle,
+      acceptAction,
+      dismissAction,
+      events,
+      token,
+      id,
+      requireMasterSecret
+    } = this.props;
+    console.log(this.props);
     return (
       <div className={"Event"}>
-        <p>{acceptAction}</p>
-        <p onClick={() => handle(acceptAction, events, id, token)}>Accept</p>
-        {/* <p onClick={() => handle(rejectURL, idx, events)}>Reject</p> */}
+        <div className="EventBody">
+          <p>{acceptAction}</p>
+          {requireMasterSecret ? (
+            <div>
+              <input
+                className="Input__Text"
+                type="text"
+                value={this.state.masterSecret}
+                onChange={e => this.setState({ masterSecret: e.target.value })}
+                placeholder="Master Secret"
+              />{" "}
+              <hr />
+            </div>
+          ) : null}
+        </div>
+        <div className="EventActions">
+          <div
+            className={
+              this.props.requireMasterSecret && !this.state.masterSecret
+                ? "Action__Accept__Disabled"
+                : "Action__Accept"
+            }
+            onClick={() =>
+              this.props.requireMasterSecret && !this.state.masterSecret
+                ? null
+                : handle(acceptAction, events, id, token)
+            }
+          >
+            <i className="fa fa-check" />
+          </div>
+          <div
+            className="Action__Dismiss"
+            onClick={() => handle(dismissAction, events, id, token)}
+          >
+            <i className="fa fa-times" />
+          </div>
+        </div>
       </div>
     );
   }
 }
-// needs container -> maps dispatch to props
-// index is props.idx
-// remove itself from state
-// dispatch -> fetch with url from props
 
 export default NotificationEvent;
