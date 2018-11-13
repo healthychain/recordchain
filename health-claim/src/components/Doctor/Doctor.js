4acd83586ad371325db4d1b5939f6e88f728c662
Boris Barath <boris.barath16@imperial.ac.uk>
2018-11-13 12:46:55 +0000

    remove dead code and update snapshot

--- health-claim/src/components/Doctor/Doctor.js
+++ health-claim/src/components/Doctor/Doctor.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import "./Doctor.scss";
 import EventPanel from "../EventPanel/EventPanel";
-import { apiEndpoint } from "../../apiEndpoint";
 import IssueBox from "./IssueBox.js";
 
 class Doctor extends Component {
@@ -20,52 +19,13 @@ class Doctor extends Component {
     }
   }
 
-  // componentDidMount() {
-  //   const { sessionID } = this.props;
-  //   this.props.fetchNotifications(sessionID);
-  // }
-
   render() {
-    // console.log(JSON.stringify(this.state.data));
-    // console.log(this.state.data);
     return (
       <div className="doctor-layout">
         <div className="doctor-main">
           <div className="doctor-inner-alt">
             <div className="Box">
-              {/* <h3>Issue a new record</h3>
-              <label className="Input__Label">Patient's username</label>
-              <input
-                onChange={e => this.setState({ username: e.target.value })}
-                className="Input__Text"
-                type="text"
-                value={this.state.username}
-              />
-              <br /> <label className="Input__Label">Health record</label> */}
               <IssueBox callback={this.props.storeRecord} />
-              {/* <hr />
-              <button
-                onClick={() => {
-                  fetch(
-                    `${apiEndpoint}/credential_offer?token=${
-                      this.props.sessionID
-                    }&prover_username=${
-                      this.state.username
-                    }&data=${JSON.stringify(this.state.data)}`
-                  );
-                }}
-                className="Button Button__Green"
-              >
-                Issue credentials
-              </button>
-              <button
-                onClick={() =>
-                  this.props.fetchNotifications(this.props.sessionID)
-                }
-                className="Button Button__Green"
-              >
-                Refresh
-              </button> */}
               <button
                 onClick={() =>
                   this.props.fetchNotifications(this.props.sessionID)
@@ -86,58 +46,58 @@ class Doctor extends Component {
       </div>
     );
 
-    return (
-      <div className="doctor-layout">
-        <div className="doctor-main">
-          <div className="doctor-inner-alt">
-            <div className="Box">
-              <label className="Input__Label">Your username</label>
-              <input
-                onChange={e => this.setState({ username: e.target.value })}
-                className="Input__Text"
-                type="text"
-                value={this.state.username}
-              />
-              <br /> <label className="Input__Label">Input</label>
-              <textarea
-                onChange={e => this.setState({ input: e.target.value })}
-                className="Input__Area"
-                type="text"
-                value={this.state.input}
-              />
-              <hr />
-              <button
-                onClick={() =>
-                  fetch(
-                    `${apiEndpoint}/credential_offer?token=${
-                      this.props.sessionID
-                    }&prover_username=${this.state.username}
-                    &data=${JSON.stringify(this.state.input)}`
-                  )
-                }
-                className="Button Button__Green"
-              >
-                Issue credentials
-              </button>
-              <button
-                onClick={() =>
-                  this.props.fetchNotifications(this.props.sessionID)
-                }
-                className="Button Button__Green"
-              >
-                Refresh
-              </button>
-            </div>
-            <br />
-          </div>
-        </div>
-        <EventPanel
-          loading={this.props.notificationsLoading}
-          error={this.props.notificationsError}
-          events={this.props.notifications}
-        />
-      </div>
-    );
+    // return (
+    //   <div className="doctor-layout">
+    //     <div className="doctor-main">
+    //       <div className="doctor-inner-alt">
+    //         <div className="Box">
+    //           <label className="Input__Label">Your username</label>
+    //           <input
+    //             onChange={e => this.setState({ username: e.target.value })}
+    //             className="Input__Text"
+    //             type="text"
+    //             value={this.state.username}
+    //           />
+    //           <br /> <label className="Input__Label">Input</label>
+    //           <textarea
+    //             onChange={e => this.setState({ input: e.target.value })}
+    //             className="Input__Area"
+    //             type="text"
+    //             value={this.state.input}
+    //           />
+    //           <hr />
+    //           <button
+    //             onClick={() =>
+    //               fetch(
+    //                 `${apiEndpoint}/credential_offer?token=${
+    //                   this.props.sessionID
+    //                 }&prover_username=${this.state.username}
+    //                 &data=${JSON.stringify(this.state.input)}`
+    //               )
+    //             }
+    //             className="Button Button__Green"
+    //           >
+    //             Issue credentials
+    //           </button>
+    //           <button
+    //             onClick={() =>
+    //               this.props.fetchNotifications(this.props.sessionID)
+    //             }
+    //             className="Button Button__Green"
+    //           >
+    //             Refresh
+    //           </button>
+    //         </div>
+    //         <br />
+    //       </div>
+    //     </div>
+    //     <EventPanel
+    //       loading={this.props.notificationsLoading}
+    //       error={this.props.notificationsError}
+    //       events={this.props.notifications}
+    //     />
+    //   </div>
+    // );
   }
 }
 
