df7b6b3d7e6951d16068460be6de2fb04fdb9eb7
malinamusuroi <mmm616@ic.ac.uk>
2018-11-15 00:40:33 +0000

    Update action name

--- health-claim/src/components/EventPanel/NotificationEvent.js
+++ health-claim/src/components/EventPanel/NotificationEvent.js
@@ -21,10 +21,13 @@ class NotificationEvent extends Component {
     const { masterSecret } = this.state;
     console.log(this.state);
     console.log(this.props);
+    var action= acceptAction.substr(0, acceptAction.indexOf('?') + 1); 
+    if(action == "create_patient_approve?") action = "Confirm this patient enrolment";
+    var eventId= acceptAction.substr(acceptAction.indexOf('?') + 1, acceptAction.length); 
     return (
       <div className={"Event"}>
         <div className="EventBody">
-          <p>{acceptAction}</p>
+          <p>{action} {eventId}</p>
           {requireMasterSecret ? (
             <div>
               <input
@@ -51,7 +54,7 @@ class NotificationEvent extends Component {
                 : handle(acceptAction, events, id, token, masterSecret)
             }
           >
-            <i className="fa fa-check" />
+             <i className="fa fa-check" />
           </div>
           <div
             className="Action__Dismiss"
