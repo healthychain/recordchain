7125fc2a284720c035fd87f6cf51f5214fd68baa
jedrazb <jedrazb@gmail.com>
2019-01-03 18:39:17 +0100

    Update genesis file

--- health-claim/src/apiEndpoint.js
+++ health-claim/src/apiEndpoint.js
@@ -1,2 +1,2 @@
 // export const apiEndpoint = window.location.origin.slice(0, -4) + "8000";
-export const apiEndpoint = "http://localhost/" + "8000";
+export const apiEndpoint = "http://localhost:" + "8000";
